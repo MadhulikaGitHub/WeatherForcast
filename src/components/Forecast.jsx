1848df8aa32b78074842dfe32d8c2a8732e1963c
MadhulikaGitHub <er.madhulikasharma@gmail.com>
2024-04-15 15:38:08 +0530

    Next Commit

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -25,69 +25,11 @@ function Forecast({ title, items }) {
                             className='w-12 my-1'
                         />
                         <p className='font-medium'>
-                        {`${item.temp.toFixed()}°`}
+                            {`${item.temp.toFixed()}°`}
                         </p>
                     </div>
 
                 ))}
-
-
-
-                {/* <div className='flex flex-col items-center justify-center'>
-                <p className='font-light text-sm'>
-                    04:30 PM
-                </p>
-                <img 
-                    src="http://openweathermap.org/img/wn/01d@2x.png" 
-                    alt=""
-                    className='w-12 my-1'
-                    />
-                <p className='font-medium'>
-                    22°
-                </p>
-            </div>
-
-            <div className='flex flex-col items-center justify-center'>
-                <p className='font-light text-sm'>
-                    04:30 PM
-                </p>
-                <img 
-                    src="http://openweathermap.org/img/wn/01d@2x.png" 
-                    alt=""
-                    className='w-12 my-1'
-                    />
-                <p className='font-medium'>
-                    22°
-                </p>
-            </div>
-
-            <div className='flex flex-col items-center justify-center'>
-                <p className='font-light text-sm'>
-                    04:30 PM
-                </p>
-                <img 
-                    src="http://openweathermap.org/img/wn/01d@2x.png" 
-                    alt=""
-                    className='w-12 my-1'
-                    />
-                <p className='font-medium'>
-                    22°
-                </p>
-            </div>
-
-            <div className='flex flex-col items-center justify-center'>
-                <p className='font-light text-sm'>
-                    04:30 PM
-                </p>
-                <img 
-                    src="http://openweathermap.org/img/wn/01d@2x.png" 
-                    alt=""
-                    className='w-12 my-1'
-                    />
-                <p className='font-medium'>
-                    22°
-                </p>
-            </div> */}
 
             </div>
 
